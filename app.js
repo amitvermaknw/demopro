18884f190c013e892c92aca93e01c3016635dd3c
Amit Verma <amit_v@zoho.com>
2016-10-09 17:02:12 -0700

    Master Pro

---
+++ app.js
@@ -0,0 +1,61 @@
+﻿
+var app = angular.module('AngularAuthApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar']);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/examples/app/views/home.html"
+    });
+
+    $routeProvider.when("/login", {
+        controller: "loginController",
+        templateUrl: "/examples/app/views/login.html"
+    });
+
+    $routeProvider.when("/signup", {
+        controller: "signupController",
+        templateUrl: "/examples/app/views/signup.html"
+    });
+
+    $routeProvider.when("/orders", {
+        controller: "ordersController",
+        templateUrl: "/examples/app/views/orders.html"
+    });
+
+    $routeProvider.when("/refresh", {
+        controller: "refreshController",
+        templateUrl: "/examples/app/views/refresh.html"
+    });
+
+    $routeProvider.when("/tokens", {
+        controller: "tokensManagerController",
+        templateUrl: "/examples/app/views/tokens.html"
+    });
+
+    $routeProvider.when("/associate", {
+        controller: "associateController",
+        templateUrl: "/examples/app/views/associate.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
+
+});
+
+//var serviceBase = 'http://localhost:26264/';
+//var serviceBase = 'http://localhost/examples/';
+var serviceBase = 'http://patientportalauthapi.somee.com/';
+app.constant('ngAuthSettings', {
+    apiServiceBaseUri: serviceBase,
+    clientId: 'ngAuthApp'
+});
+
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptorService');
+});
+
+app.run(['authService', function (authService) {
+    authService.fillAuthData();
+}]);
+
+
